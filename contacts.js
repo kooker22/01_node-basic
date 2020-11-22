6fffe5f9610ab93ec8bc128af7a0a2c9b2e542ea
Иван Попович <kooker22>
2020-11-22 16:45:42 +0200

    changed async/await fn

--- contacts.js
+++ contacts.js
@@ -3,58 +3,68 @@ const path = require('path');
 const { number } = require('yargs');
 const contactsPath = path.resolve(__dirname, 'db', 'contacts.json');
 
+// module.exports.listContacts= async function() {
+//     const contacts = await fs
+//     .readFile(contactsPath, {encoding: 'utf-8'})
+//     .then((data) => JSON.parse(data));
+//     console.table(contacts)
+// };
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, { encoding: 'utf-8' });
+    const parsedData = await JSON.parse(data);
+    console.table(parsedData);
+  } catch (error) {
+    console.error(error);
+  }
+}
+async function getContactById(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath, { encoding: 'utf-8' });
+    const parsedData = await JSON.parse(data);
+    const findedContact = await parsedData.find(
+      (contact) => contact.id === contactId
+    );
+    console.table(findedContact);
 
-module.exports.listContacts= async function() {
-    const contacts = await fs
-    .readFile(contactsPath, {encoding: 'utf-8'})
-    .then((data) => JSON.parse(data));
-    console.table(contacts)
-};
-  
-module.exports.getContactById = async function (contactId) {
-    try {
-      const contacts = await fs
-        .readFile(contactsPath, { encoding: 'utf-8' })
-        .then((data) => JSON.parse(data));
-      const findedContact = contacts.find((contact) => contact.id === contactId);
-      console.table(findedContact);
-  
-      return findedContact;
-    } catch (error) {
-      console.log('error', error);
-    }
-  };
-  
-  module.exports.removeContact = async function (contactId) {
-    const contacts = await fs
-      .readFile(contactsPath, { encoding: 'utf-8' })
-      .then((data) => JSON.parse(data));
-    const filteredList = contacts.filter((contact) => contact.id !== contactId);
+    return findedContact;
+  } catch (error) {
+    console.log('error', error);
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath, { encoding: 'utf-8' });
+    const parsedData = await JSON.parse(data);
+    const filteredList = await parsedData.filter(
+      (contact) => contact.id !== contactId
+    );
     const FilteredListAsJSON = JSON.stringify(filteredList);
     console.table(filteredList);
-    fs.writeFile(contactsPath, FilteredListAsJSON, (err) => {
-      if (err) throw err;
-      return err;
-    });
-  };
-  
-  module.exports.addContact = async function (name, email, phone) {
-    const contacts = await fs
-      .readFile(contactsPath, { encoding: 'utf-8' })
-      .then((data) => JSON.parse(data));
-    const lastElement = contacts.pop();
-    contacts.push(lastElement);
-    const id = lastElement.id + 1;
-  
+    await fs.writeFile(contactsPath, FilteredListAsJSON, 'utf-8');
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const data = await fs.readFile(contactsPath, { encoding: 'utf-8' });
+    const parsedData = await JSON.parse(data);
+    const lastElement = await parsedData.pop();
+    await parsedData.push(lastElement);
+    const id = (await lastElement.id) + 1;
+
     const newContact = {
       id: id,
       name,
       email,
       phone,
     };
-  
-    contacts.push(newContact);
-    contacts.sort(function (a, b) {
+
+    await parsedData.push(newContact);
+    await parsedData.sort(function (a, b) {
       if (a.id > b.id) {
         return 1;
       }
@@ -63,8 +73,12 @@ module.exports.getContactById = async function (contactId) {
       }
       return 0;
     });
-    const newContactListAsJSON = JSON.stringify(contacts);
-    fs.writeFile(contactsPath, newContactListAsJSON);
-    console.table(contacts);
-  };
-  
+    const newContactListAsJSON = JSON.stringify(parsedData);
+    await fs.writeFile(contactsPath, newContactListAsJSON);
+    console.table('Contact has been added!');
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
